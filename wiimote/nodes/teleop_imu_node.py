9a88778b0ca3d8a659362cb4c4b2d3213208984a
Ryusei Baba <babaryusei.kw@gmail.com>
2024-10-14 21:40:17 +0900

    Joy操作を追加

--- wiimote/nodes/teleop_imu_node.py
+++ wiimote/nodes/teleop_imu_node.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import Imu
-import math
+from sensor_msgs.msg import Imu, Joy
 
 class TeleopImuNode:
     def __init__(self):
         rospy.init_node('teleop_imu_node', anonymous=True)
 
-        # IMUデータのサブスクライバを設定
-        rospy.Subscriber('/imu/data', Imu, self.imu_callback)
-
-        # /cmd_velパブリッシャを設定
-        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        # yamlファイルからボタンマッピングと速度設定を読み込む
+        self.button_mapping = rospy.get_param('~button_mapping')
+        self.linear_speeds = rospy.get_param('~linear_speeds')
 
         # 初期化
         self.vel = Twist()
         self.vel.linear.x = 0.0  # 並進速度
         self.vel.angular.z = 0.0  # 角速度
+
+        # IMUデータのサブスクライバを設定
+        rospy.Subscriber('/imu/data', Imu, self.imu_callback)
+        # Joyデータのサブスクライバを設定
+        rospy.Subscriber('/joy', Joy, self.joy_callback)
+
+        # /cmd_velパブリッシャを設定
+        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
     def map_value(self, value, in_min, in_max, out_min, out_max):
         # 線形マッピングの公式
@@ -27,16 +32,29 @@ class TeleopImuNode:
         # IMUの線形加速度のy成分を取得
         linear_accel_y = imu_data.linear_acceleration.y
 
-        # IMUの線形加速度yを -10から10の範囲から -πからπの範囲にマッピング
-        mapped_angular_z = self.map_value(linear_accel_y, -10, 10, -math.pi, math.pi)
+        # IMUの線形加速度yを -10から10の範囲から -1から1の範囲にマッピング
+        mapped_angular_z = self.map_value(linear_accel_y, -10, 10, -1, 1)
 
         # 角速度を設定
-        cmd_vel = Twist()
-        cmd_vel.linear.x = 0.2  # 並進速度は固定
-        cmd_vel.angular.z = mapped_angular_z  # 正規化された角速度を適用
+        self.vel.angular.z = mapped_angular_z  # 正規化された角速度を適用
 
         # 速度コマンドをパブリッシュ
-        self.cmd_vel_pub.publish(cmd_vel)
+        self.cmd_vel_pub.publish(self.vel)
+
+    def joy_callback(self, joy):
+        # yamlで指定されたボタンの状態をチェック
+        if joy.buttons[self.button_mapping['forward']] == 1:
+            self.vel.linear.x = self.linear_speeds['forward']  # 前進速度
+            rospy.loginfo(f"Speed changed to: {self.vel.linear.x} m/s")
+        elif joy.buttons[self.button_mapping['backward']] == 1:
+            self.vel.linear.x = self.linear_speeds['backward']  # 後退速度
+            rospy.loginfo(f"Speed changed to: {self.vel.linear.x} m/s")
+        else :
+            self.vel.linear.x = self.linear_speeds['stop']  # 停止速度
+            rospy.loginfo(f"Speed changed to: {self.vel.linear.x} m/s")
+
+        # 速度コマンドをパブリッシュ
+        self.cmd_vel_pub.publish(self.vel)
 
 if __name__ == '__main__':
     try:
